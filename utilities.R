40d749274c0907b0b2324eda9149fd318d8ee684
branden-ciranni <branden.ciranni@gmail.com>
2020-11-26 19:27:23 -0500

    null values exploration

--- utilities.R
+++ utilities.R
@@ -1,5 +1,21 @@
+# Utility functions, left out of the main `analysis.Rmd` notebook for cleanliness.
 
 # count the number of null values in each column of the DataFrame
 count_null <- function(df) {
-  sapply(df, function(x) sum(is.na(x)))
+  null.count <- sapply(df, function(x) sum(is.na(x)))
+  null.perc <- sapply(df, function(x) sum(is.na(x))/nrow(df))
+  df.null <- data.frame(feature=names(null.count),
+                        null.count=null.count,
+                        null.perc=null.perc) 
+  return (df.null %>% arrange(desc(null.perc)))
 }
+
+
+# plot percentage graph of missing values
+plot_missing_values <- function(df) {
+  df.null = count_null(df)
+  null.plot <- ggplot(df.null) + 
+                geom_col(aes(x=reorder(feature, desc(null.perc)), y=null.perc))
+  return (null.plot)
+}
+
